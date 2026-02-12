c12ffe6d52fb23621b4d915c4dcd350adf970178
RemiLequette <remi.lequette@laposte.net>
2026-02-12 18:58:10 +0100

    Remise a zero

--- visualization/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-DDoptim Visualization Module
-
-Interactive web-based visualization tools for supply chain networks.
-"""
-
-from .network_viewer import NetworkVisualizer
-
-__all__ = ['NetworkVisualizer']
